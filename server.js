611f8d503170e53aca726966416256a122b4d2db
Yevheniia Piatkova <piatkovaes@gmail.com>
2022-10-31 20:20:09 +0100

    My first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controlers/register');
+const signin = require('./controlers/signin');
+const profile = require('./controlers/profile');
+const image = require('./controlers/image');
+
+
+const db = knex({
+	client: 'pg',
+	connection: {
+	host : '127.0.0.1', //localhost
+	user : 'postgres', //add your user name for the database here
+	password : 'test', //add your correct password in here
+	database : 'smart-brain' //add your database name you created here
+	}
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send('it is working!') })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) })
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('app is running on port ${process.env.PORT}');
+})
