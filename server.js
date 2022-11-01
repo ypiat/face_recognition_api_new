73ca1f5adb6163f5a6999dd3376132374e4b8519
Yevheniia Piatkova <piatkovaes@gmail.com>
2022-11-01 12:24:29 +0100

    fix

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const image = require('./controlers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-	connectionString: process.env.DATABASE_URL,
-	ssl: true
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
