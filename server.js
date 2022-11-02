79c748c6cf607a8aa4ecc16cd7d3d88ddf0ed251
Yevheniia Piatkova <piatkovaes@gmail.com>
2022-11-02 15:15:00 +0100

    fixing connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
