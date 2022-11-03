2d9cb068bce49cfd10fcc61f8e6ca7145f076904
Yevheniia Piatkova <piatkovaes@gmail.com>
2022-11-03 08:40:40 +0100

    ipdate

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: {
-          rejectUnauthorized: false
-        }
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smart-brain'
   }
 });
 
